d2cefb143c0d0a01804a3cbf9852c9c60c3ca47e
Pulkit Chopra <65125133+Pulkitchopra@users.noreply.github.com>
2023-10-04 19:59:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,20 +11,28 @@ import cors from 'cors'
 
 
 
+import { fileURLToPath } from 'url';
 import path from 'path';
 const app = express();
+
 dotenv.config();
 
 database();
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 app.use(cors())
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname, './frontend/build')))
+
+
+
 app.use('/api/v1/auth', authRoutes);
+
 app.use('/api/v1/category', categoryRoutes);
 app.use("/api/v1/product", productRoutes);
-
-
 
 
 app.use('*', function(req,res){
@@ -32,12 +40,16 @@ app.use('*', function(req,res){
     res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
 });
 
-
 app.get('/', (req,res) => {
     res.send({
         message: 'ECommerce Project'
     })
 })
+
+
+
+
+
 
 const PORT = process.env.PORT || 5000;
 
